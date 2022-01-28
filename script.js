94963ff64e584c7858308bf09ccc575835807fc7
Ogu <august.gros@gmail.com>
2022-01-28 11:53:40 +0100

    fin du darkmode/lightmode

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ btnTheme.addEventListener("click", function(){
     let body = document.getElementById("body");
     let currentClass = body.className;
     body.className = currentClass == "darkMode" ? "lightMode" : "darkMode";
-    if (currentClass == "darkMode"){
-        
+    if (currentClass == "lightMode"){
+        document.querySelector('.meteo').classList.add('fa-sun');
+        document.querySelector('.meteo').classList.remove('fa-moon');
+        let textmode = document.querySelector(".mode").innerHTML; 
+        document.querySelector('.mode').innerHTML = textmode.replace("DARK", "LIGHT");
+    }else{
+        document.querySelector('.meteo').classList.add('fa-moon');
+        document.querySelector('.meteo').classList.remove('fa-sun');
+        let textmode = document.querySelector(".mode").innerHTML; 
+        document.querySelector('.mode').innerHTML = textmode.replace("LIGHT", "DARK");
     }
 })
 
