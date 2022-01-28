775f52b5b9283f412241005550e1916b7eb50987
Ogu <august.gros@gmail.com>
2022-01-28 11:32:11 +0100

    add darkmode

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+/* Light/Dark mode Toggle */
+const btnTheme = document.querySelector('.btnTheme');
+
+btnTheme.addEventListener("click", function(){
+    let body = document.getElementById("body");
+    let currentClass = body.className;
+    body.className = currentClass == "darkMode" ? "lightMode" : "darkMode";
+    if (currentClass == "darkMode"){
+        
+    }
+})
+
+
 /* btn search + error */
